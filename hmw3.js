ecd6fcbaa7b5921f377f3d3e14ced1dbcdc426db
selma-Bentaiba <99991684+selma-Bentaiba@users.noreply.github.com>
2022-03-22 14:14:35 -0700

    Create hmw3.js

---
+++ hmw3.js
@@ -0,0 +1,77 @@
+
+/*      Exercice 1:
+        Write a JavaScript program that compare two objects 
+        to determine if the first one contains equivalent 
+        property values to the second one.
+*/
+/*
+var favorite ={
+    movie : " Back to the future",
+    book: "	Skinnybones",
+    app :"Pinterest",
+    fruit: "watermelon" ,
+
+}
+var favorite2={
+    movie : " Harry Potter ",
+    book: "Animal farm",
+    app :"Pinterest",
+    fruit: "watermelon" ,
+
+}
+var favorite3={
+    app :"Pinterest",
+    fruit: "watermelon" ,
+}
+function compare(obj1,obj2 ){
+
+for(key in obj2){
+
+    if(obj1[key]!==obj2[key]){
+        return false;
+    }
+}    return true;
+
+}
+
+console.log(compare(favorite,favorite2));
+console.log(compare(favorite,favorite3));
+console.log(compare(favorite2,favorite3));
+
+
+
+
+*/
+
+
+
+      /*   Exercice 2:
+        Write a JavaScript function to find the common elements from two arrays
+        console.log(difference([1, 2, 3], [100, 2, 1, 10]));
+        ["1", "2"] */
+        function difference(arr1, arr2) {
+            var common = [];                   // Array to contain common elements
+            for(var i=0 ; i<arr1.length ; ++i) {
+              for(var j=0 ; j<arr2.length ; ++j) {
+                if(arr1[i] == arr2[j]) {       // If element is in both the arrays
+                  common.push(arr1[i]);        // Push to common array
+                }
+              }
+            }
+            
+            return common;                     // Return the common elements
+          }
+          var A= [1, 2, 3];
+          var B= [100, 2, 1, 10];
+          // Get common elements of arr1, arr2
+          var commonElements = difference(A, B); 
+          console.log(commonElements);
+
+
+
+
+
+
+
+
+
